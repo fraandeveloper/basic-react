4e45949173b62d850f039a9198cfdf681b329f90
Francisco barros <franciscobarros754@gmail.com>
2021-02-07 10:15:28 -0300

    Renderindo h1 no com virtual DOM react

--- js/jsx.js
+++
@@ -1,9 +0,0 @@
-const rootContainer = document.getElementById('root');
-
-function Component1(props) {
-  return <h1>Francisco</h1>
-}
-
-const element = <Component1 />
-
-ReactDOM.render(element, rootContainer);
